e1772402e5953d7852b0b709785b5669c51a4c64
SayFu11a <sgishlakaev@mail.ru>
2023-05-18 08:00:10 +0300

    update

---
+++ src/components/Categories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categ, index) => (
+          <li
+            key={index}
+            onClick={() => setActiveIndex(index)}
+            className={activeIndex == index ? 'active' : ''}>
+            {categ}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
