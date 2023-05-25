45819edc862bf69bddb3071ffcd44f598ff8b94d
SayFu11a <sgishlakaev@mail.ru>
2023-05-25 09:52:35 +0300

    fixed

--- src/components/PizzaBlock/Skeleton.jsx
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -1,22 +1,21 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react';
+import ContentLoader from 'react-content-loader';
 
-const Skeleton = (props) => (
-  <ContentLoader 
+const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
     speed={2}
     width={280}
     height={500}
     viewBox="0 0 280 500"
     backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...props}
-  >
-    <rect x="0" y="407" rx="10" ry="10" width="95" height="30" /> 
-    <rect x="0" y="281" rx="10" ry="10" width="280" height="23" /> 
-    <rect x="0" y="329" rx="5" ry="5" width="280" height="55" /> 
-    <circle cx="134" cy="134" r="134" /> 
-    <rect x="130" y="400" rx="25" ry="25" width="152" height="42" />
+    foregroundColor="#ecebeb">
+    <circle cx="134" cy="136" r="125" />
+    <rect x="0" y="279" rx="10" ry="10" width="280" height="23" />
+    <rect x="0" y="326" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="436" rx="10" ry="10" width="95" height="30" />
+    <rect x="125" y="427" rx="24" ry="24" width="152" height="45" />
   </ContentLoader>
-)
+);
 
-export default Skeleton
+export default Skeleton;
