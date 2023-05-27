d5b3482de5786f2b2bc1ba7fef9a9c3704ae6ffb
SayFu11a <sgishlakaev@mail.ru>
2023-05-27 17:53:01 +0300

    add react-roter-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://6465cabb9c09d77a62f404da.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
